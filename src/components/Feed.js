9162f5d5c878ac1719194d476dfc8ad37417a968
deniz Ay <denizay@std.iyte.edu.tr>
2022-03-22 14:43:49 +0300

    Post Component added

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -8,6 +8,7 @@ import {
 } from "@material-ui/icons";
 import "./Feed.css";
 import InputOption from "./InputOption";
+import Post from "./Post";
 
 const Feed = () => {
   return (
@@ -20,17 +21,22 @@ const Feed = () => {
             <button type="submit">Send</button>
           </form>
         </div>
+        <div className="feed__inputOptions">
+          <InputOption Icon={Image} title="Photo" color="#70B5F9" />
+          <InputOption Icon={Subscriptions} title="Video" color="#E7A33E" />
+          <InputOption Icon={EventNote} title="Event" color="#C0CBCD" />
+          <InputOption
+            Icon={CalendarViewDay}
+            title="Write Article"
+            color="#7FC15E"
+          />
+        </div>
       </div>
-      <div className="feed__inputOptions">
-        <InputOption Icon={Image} title="Photo" color="#70B5F9" />
-        <InputOption Icon={Subscriptions} title="Video" color="#E7A33E" />
-        <InputOption Icon={EventNote} title="Event" color="#C0CBCD" />
-        <InputOption
-          Icon={CalendarViewDay}
-          title="Write Article"
-          color="#7FC15E"
-        />
-      </div>
+      <Post
+        name="Deniz AY"
+        description="This is a test"
+        message="This is awesome thing to do"
+      />
     </div>
   );
 };
---
+++ src/components/Post.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Avatar } from "@material-ui/core";
+import {
+  ChatOutlined,
+  SendOutlined,
+  ShareOutlined,
+  ThumbUpAltOutlined,
+} from "@material-ui/icons";
+import InputOption from "./InputOption";
+import "./Post.css";
+
+const Post = ({ name, description, message, photoUrl }) => {
+  return (
+    <div className="post">
+      <div className="post__header">
+        <Avatar />
+        <div className="post__info">
+          <h2>{name}</h2>
+          <p>{description}</p>
+        </div>
+      </div>
+      <div className="pos__body">
+        <p>{message}</p>
+      </div>
+      <div className="post__buttons">
+        <InputOption Icon={ThumbUpAltOutlined} title="Like" color="gray" />
+        <InputOption Icon={ChatOutlined} title="Comment" color="gray" />
+        <InputOption Icon={ShareOutlined} title="Share" color="gray" />
+        <InputOption Icon={SendOutlined} title="Send" color="gray" />
+      </div>
+    </div>
+  );
+};
+
+export default Post;
