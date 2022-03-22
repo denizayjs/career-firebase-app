b9a9592cc14e655ed7e9f416936b8019bde33dcd
deniz Ay <denizay@std.iyte.edu.tr>
2022-03-22 14:07:07 +0300

    Feed Component added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
+import Feed from "./components/Feed";
 import "./App.css";
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
       <Header />
       <div className="app__body">
         <Sidebar />
-        <div></div>
+        <Feed />
       </div>
     </div>
   );
