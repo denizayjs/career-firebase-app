ccd15c9b80046e6fe7690b3bb2a9edd1f28ce4b7
deniz Ay <denizay@std.iyte.edu.tr>
2022-03-26 15:13:09 +0300

    getPost and sendPost added

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   CalendarViewDay,
   Create,
@@ -9,16 +9,56 @@ import {
 import "./Feed.css";
 import InputOption from "./InputOption";
 import Post from "./Post";
+import { db } from "../firebase";
+import {
+  getDocs,
+  collection,
+  addDoc,
+  serverTimestamp,
+} from "firebase/firestore";
 
 const Feed = () => {
+  const [posts, setPosts] = useState([]);
+  const [input, setInput] = useState("");
+  useEffect(() => {
+    getPosts();
+  }, []);
+  const getPosts = () => {
+    getDocs(collection(db, "posts")).then((item) => {
+      setPosts(
+        item.docs.map((doc) => ({
+          id: doc.id,
+          item: doc.data(),
+        }))
+      );
+    });
+  };
+
+  const sendPost = (e) => {
+    e.preventDefault();
+    addDoc(collection(db, "posts"), {
+      name: "Deniz AY",
+      description: "This is test",
+      message: input,
+      photoUrl: "",
+      timestamp: serverTimestamp(),
+    });
+    getPosts();
+  };
   return (
     <div className="feed">
       <div className="feed__inputContainer">
         <div className="feed__input">
           <Create />
           <form>
-            <input type="text" />
-            <button type="submit">Send</button>
+            <input
+              type="text"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
+            <button type="submit" onClick={sendPost}>
+              Send
+            </button>
           </form>
         </div>
         <div className="feed__inputOptions">
@@ -32,11 +72,15 @@ const Feed = () => {
           />
         </div>
       </div>
-      <Post
-        name="Deniz AY"
-        description="This is a test"
-        message="This is awesome thing to do"
-      />
+      {posts?.map(({ id, item }) => (
+        <Post
+          key={id}
+          name={item.name}
+          description={item.description}
+          message={item.message}
+          photoUrl={item.photoUrl}
+        />
+      ))}
     </div>
   );
 };
--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,3 +18,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+//const auth = getAuth();
+//const provider = new GoogleAuthProvider();
+
+export { db };
